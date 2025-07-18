da121e94f96430f923d71494de0f8a08c63a462b
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2025-07-18 14:23:26 +1000

    Tag: v1.2 compile-able env.
    *Note: `nimble deploy` would not work, only `nimble release` or similar commands.*

--- shell.nix
+++ shell.nix
@@ -5,11 +5,11 @@ let
   nixpkgs-nim = fetchTarball "https://github.com/NixOS/nixpkgs/archive/a71323f68d4377d12c04a5410e214495ec598d4c.tar.gz";
   pkgs-nim = import nixpkgs-nim {};
 in pkgs.mkShellNoCC {
-  packages = with pkgs; [
+  packages = with pkgs-nim; [
     nim_lk
     gcc12
 
-    pkgs-nim.nim
+    nim
 
     xorg.libX11
     xorg.libXcursor
@@ -18,6 +18,5 @@ in pkgs.mkShellNoCC {
     xorg.libXi
     libGL
     xorg.libXxf86vm
-
   ];
 }

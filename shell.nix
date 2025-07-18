6f9f965c7478c5443c4559206f486d944ef9c978
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2025-07-18 16:29:02 +1000

    Env: pure dev-shell.

--- shell.nix
+++ shell.nix
@@ -2,21 +2,43 @@ let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
   pkgs = import nixpkgs {};
 
-  nixpkgs-nim = fetchTarball "https://github.com/NixOS/nixpkgs/archive/a71323f68d4377d12c04a5410e214495ec598d4c.tar.gz";
-  pkgs-nim = import nixpkgs-nim {};
+  nixpkgs-lock = fetchTarball "https://github.com/NixOS/nixpkgs/archive/a71323f68d4377d12c04a5410e214495ec598d4c.tar.gz";
+  pkgs-lock = import nixpkgs-lock {};
 in pkgs.mkShellNoCC {
-  packages = with pkgs-nim; [
-    nim_lk
-    gcc12
+  packages = [
+    pkgs.git
+    pkgs.cacert
 
-    nim
+    pkgs.nimble
+    pkgs.nim_lk
 
-    xorg.libX11
-    xorg.libXcursor
-    xorg.libXrandr
-    xorg.libXinerama
-    xorg.libXi
-    libGL
-    xorg.libXxf86vm
+    pkgs-lock.gcc12
+    pkgs-lock.nim
+
+    pkgs-lock.xorg.libX11
+    pkgs-lock.xorg.libXcursor
+    pkgs-lock.xorg.libXrandr
+    pkgs-lock.xorg.libXinerama
+    pkgs-lock.xorg.libXi
+    pkgs-lock.libGL
+    pkgs-lock.xorg.libXxf86vm
   ];
+
+  shellHook = ''
+    IMPURE_CACHE=false
+
+    if [ -d "$NIMBLE_DIR" ]; then
+      export PATH="$PATH:/home/$NIMBLE_DIR/bin"
+      echo "Using env NIMBLE_DIR might cause inconsistent behaviour"
+
+      if [ ! -z "$(ls $NIMBLE_DIR)" ]; then IMPURE_CACHE=true; fi
+    else
+      DEFAULT_NIMBLE_DIR="/home/$(whoami)/.nimble"
+      export PATH="$PATH:$DEFAULT_NIMBLE_DIR/bin"
+
+      if [ ! -z "$(ls $DEFAULT_NIMBLE_DIR)" ]; then IMPURE_CACHE=true; fi
+    fi
+
+    [ $IMPURE_CACHE ] && echo "Existing Nimble cache might cause inconsistent behaviour"
+  '';
 }

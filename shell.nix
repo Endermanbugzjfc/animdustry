5dabaf1732a1f30634a2fbefa1180e50e7a895ef
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2025-07-26 05:11:54 +1000

    Env: fixed cache warning.

--- shell.nix
+++ shell.nix
@@ -28,15 +28,15 @@ in pkgs.mkShellNoCC {
       export PATH="$PATH:/home/$NIMBLE_DIR/bin"
       echo "Specifying the NIMBLE_DIR env might cause inconsistent behaviour"
 
-      if [ ! -z "$(ls $NIMBLE_DIR)/pkgcache" ]; then IMPURE_CACHE=true; fi
+      [ -d "$NIMBLE_DIR/pkgcache" ] && IMPURE_CACHE=true
     else
       DEFAULT_NIMBLE_DIR="/home/$(whoami)/.nimble"
       export PATH="$PATH:$DEFAULT_NIMBLE_DIR/bin"
 
-      if [ ! -z "$(ls $DEFAULT_NIMBLE_DIR)/pkgcache" ]; then IMPURE_CACHE=true; fi
+      [ -d "$DEFAULT_NIMBLE_DIR/pkgcache" ] && IMPURE_CACHE=true
     fi
 
-    [ $IMPURE_CACHE ] && echo "Existing Nimble cache might cause inconsistent behaviour"
-    [ "$(find . -maxdepth 1 -type f | wc -l)" == "0" ] && echo "IMPORTANT: incorrect or missing submodules might cause compiler errors!"
+    [ "$IMPURE_CACHE" == "true" ] && echo "Existing Nimble cache might cause inconsistent behaviour"
+    [ "$(find fau -maxdepth 1 -type f | wc -l)" == "0" ] && echo "IMPORTANT: incorrect or missing submodules might cause compiler errors!"
   '';
 }

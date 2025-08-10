664020da7c0ca5fe8c9f540798c6b1fa58e8d0f1
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2025-08-11 09:54:12 +1000

    Env: separate shell for cross-compiling. Chore: improved warnings.

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
   pkgs = import nixpkgs {};
-in pkgs.mkShellNoCC {
+
   packages = with pkgs; [
     git
     cacert
@@ -10,7 +10,7 @@ in pkgs.mkShellNoCC {
     nimble
     nim_lk
 
-    nim
+    # nim
 
     xorg.libX11
     xorg.libXcursor
@@ -21,22 +21,57 @@ in pkgs.mkShellNoCC {
     xorg.libXxf86vm
   ];
 
-  shellHook = ''
-    IMPURE_CACHE=false
+  shellHook = let
+    warn_nimble_dir = "Specifying the NIMBLE_DIR env might cause inconsistent behaviour, it is currently set to: $NIMBLE_DIR";
+    warn_cache = "Existing Nimble cache might cause inconsistent behaviour: $CACHE_PATH";
+    warn_submodules = "IMPORTANT: incorrect or missing submodules might cause compiler errors!" +
+      " If you are cloning with git, append \\`--recursive\\`" +
+      " If you are cloning with gh, append \\`-- --recursive\\`";
+  in ''
+    CACHE_PATH=""
 
     if [ -d "$NIMBLE_DIR" ]; then
-      export PATH="$PATH:/home/$NIMBLE_DIR/bin"
-      echo "Specifying the NIMBLE_DIR env might cause inconsistent behaviour"
+      export PATH="$PATH:/home/$NIMBLE_DIR/bin" echo ${warn_nimble_dir}
 
-      [ -d "$NIMBLE_DIR/pkgcache" ] && IMPURE_CACHE=true
+      CACHE_PATH="$NIMBLE_DIR/pkgcache"
+      [ -d "$CACHE_PATH" ] || CACHE_PATH=""
     else
       DEFAULT_NIMBLE_DIR="/home/$(whoami)/.nimble"
       export PATH="$PATH:$DEFAULT_NIMBLE_DIR/bin"
 
-      [ -d "$DEFAULT_NIMBLE_DIR/pkgcache" ] && IMPURE_CACHE=true
+      CACHE_PATH="$DEFAULT_NIMBLE_DIR/pkgcache"
+      [ -d "$CACHE_PATH" ] || CACHE_PATH=""
     fi
 
-    [ "$IMPURE_CACHE" == "true" ] && echo "Existing Nimble cache might cause inconsistent behaviour"
-    [ "$(find fau -maxdepth 1 -type f | wc -l)" == "0" ] && echo "IMPORTANT: incorrect or missing submodules might cause compiler errors!"
+    [ "$CACHE_PATH" == "" ] || echo ${warn_cache}
+    [ "$(find fau -maxdepth 1 -type f | wc -l)" == "0" ] && echo ${warn_submodules}
   '';
+
+  lin = with pkgs; mkShellNoCC {
+    packages = packages ++ [ nim ];
+
+    inherit shellHook;
+  };
+
+in {
+  inherit lin;
+  default = lin;
+
+  win = with pkgs; let
+    cross = with pkgsCross.mingwW64.buildPackages; [
+      gcc
+      nim
+    ];
+    compatibility = [
+      wine64
+      winetricks
+    ];
+  in { wineprefix ? "~/.local/share/wineprefixes/animdustry" }: mkShellNoCC {
+    packages = packages ++ cross ++ compatibility;
+
+    shellHook = shellHook + "\n" + ''
+      export WINEPREFIX="$(realpath ${wineprefix})"
+      export WINE="wine64"
+    '';
+  };
 }

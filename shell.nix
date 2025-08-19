21c8c1afdce4db0f7e6c0f69fd09628f5d9bc951
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2025-08-20 02:39:12 +1000

    Env: added alias to remove cache.

--- shell.nix
+++ shell.nix
@@ -20,29 +20,34 @@ let
   ];
 
   shellHook = let
+    removeCacheAlias = "rm-nimble";
     warn_nimble_dir = "Specifying the NIMBLE_DIR env might cause inconsistent behaviour, it is currently set to: $NIMBLE_DIR";
-    warn_cache = "Existing Nimble cache might cause inconsistent behaviour: $CACHE_PATH";
+    warn_cache = "Inconsistent behaviour might happen because of the existing Nimble cache at $CACHE_PATH; " +
+      "To quickly delete it permanently, run: ${removeCacheAlias} (any loss are at your own risk)";
     warn_submodules = "IMPORTANT: incorrect or missing submodules might cause compiler errors!" +
       " If you are cloning with git, append \\`--recursive\\`" +
       " If you are cloning with gh, append \\`-- --recursive\\`";
   in ''
+    HAS_CACHE=false
     CACHE_PATH=""
 
     if [ -d "$NIMBLE_DIR" ]; then
-      export PATH="$PATH:/home/$NIMBLE_DIR/bin" echo ${warn_nimble_dir}
+      export PATH="$PATH:/home/$NIMBLE_DIR/bin" echo "${warn_nimble_dir}"
 
       CACHE_PATH="$NIMBLE_DIR/pkgcache"
-      [ -d "$CACHE_PATH" ] || CACHE_PATH=""
+      [ -d "$CACHE_PATH" ] || HAS_CACHE=true
     else
       DEFAULT_NIMBLE_DIR="/home/$(whoami)/.nimble"
       export PATH="$PATH:$DEFAULT_NIMBLE_DIR/bin"
 
       CACHE_PATH="$DEFAULT_NIMBLE_DIR/pkgcache"
-      [ -d "$CACHE_PATH" ] || CACHE_PATH=""
+      [ -d "$CACHE_PATH" ] || HAS_CACHE=true
     fi
+    RM="rm -rf \"$CACHE_PATH\"";
+    alias ${removeCacheAlias}="echo \"$RM\" && $RM"
 
-    [ "$CACHE_PATH" == "" ] || echo ${warn_cache}
-    [ "$(find fau -maxdepth 1 -type f | wc -l)" == "0" ] && echo ${warn_submodules}
+    [ "$HAS_CACHE" == "true" ] || echo "${warn_cache}"
+    [ "$(find fau -maxdepth 1 -type f | wc -l)" == "0" ] && echo "${warn_submodules}"
 
     export LD_LIBRARY_PATH=${ with pkgs; lib.makeLibraryPath [
         libpulseaudio

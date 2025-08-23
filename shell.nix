15897971ece4dc32ccf13ee821facd0cf95ad1bf
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2025-08-24 08:04:42 +1000

    Env: replace nim_lk with nnl.

--- shell.nix
+++ shell.nix
@@ -2,13 +2,20 @@ let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
   pkgs = import nixpkgs {};
 
+  nnl = pkgs.fetchFromGitHub {
+    repo = "nnl";
+    owner = "daylinmorgan";
+    rev = "09d6bea65bf5d39e0f1f43cb3922cca60dfeb749";
+    hash = "sha256-mEvdoYLoumAFzMKPKz4CBoBX5b7AvYu5jGQnNLzEBH0=";
+  };
+
   packages = with pkgs; [
     git
     cacert
     gcc # Note: this also adds the `strip` utility.
 
     nimble
-    nim_lk
+    (callPackage "${nnl}/package.nix" {})
 
     xorg.libX11
     xorg.libXcursor
